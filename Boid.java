3f147c2ca144220fd0a11eb2f85864a5c6c0f451
michael williams <williamsmi@live.com>
2020-06-11 20:34:39 -0400

    init commit

---
+++ Boid.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Boid {
+	Vector2D position;
+	Vector2D velocity;
+  	Vector2D acceleration;
+  	float r;
+  	float maxforce;    // Maximum steering force
+  	float maxspeed;    // Maximum speed
+	
+	public Boid(double x, double y) {
+		acceleration = new Vector2D(0, 0);
+		position = new Vector2D(x, y);	
+	}
+
+	void run(ArrayList<Boid> boids) {
+		
+	}
+}
